343e850957068f7eb4da0c4184e3ea896f4a1515
Ihar Kozik <ihar.kozik@gmail.com>
2022-07-03 00:28:43 +0300

    feat: add jwt query

--- src/modules/users/users.resolver.ts
+++ src/modules/users/users.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+
+import { UsersService } from './users.service';
+import { JwtDTO } from './dto/jwt.dto';
 
 @Resolver()
-export class UsersResolver {}
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => JwtDTO)
+  async jwt(@Args('email') email: string, @Args('password') password: string) {
+    return this.usersService.login({ email, password });
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+import { LoginInputDTO } from './dto/login-input.dto';
+import { JwtDTO } from './dto/jwt.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private client;
+
+  constructor() {
+    this.client = axios.create({
+      baseURL: process.env.USERS_URL,
+    });
+  }
+
+  public async login(loginInputDTO: LoginInputDTO): Promise<JwtDTO> {
+    const response = await this.client
+      .post('/login', loginInputDTO)
+      .catch((res) => {
+        res.sendStatus(404);
+      });
+    return response?.data ? response.data : 'Not Found';
+  }
+}
