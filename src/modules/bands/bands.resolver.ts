4dc51c34f0b3c24990dc3390f41762a2b035db27
Ihar Kozik <ihar.kozik@gmail.com>
2022-07-08 15:45:11 +0300

    feat: add updateBand mutation

--- src/modules/bands/bands.resolver.ts
+++ src/modules/bands/bands.resolver.ts
@@ -39,4 +39,28 @@ export class BandsResolver {
       process.env.BANDS_URL,
     );
   }
+
+  @Mutation(() => BandCreateUpdateDTO)
+  async updateBand(
+    @Args('id') id: string,
+    @Args('name') name: string,
+    @Args('origin', { nullable: true }) origin?: string,
+    @Args('members', { nullable: true, type: () => [MemberInputDTO] })
+    members?: MemberInputDTO[],
+    @Args('website', { nullable: true }) website?: string,
+    @Args('genresIds', { nullable: true, type: () => [String] })
+    genresIds?: string[],
+  ) {
+    const inputData: BandCreateUpdateInput = { name };
+    if (origin) inputData.origin = origin;
+    if (members) inputData.members = members;
+    if (website) inputData.website = website;
+    if (genresIds) inputData.genresIds = genresIds;
+    return this.bandsService.update(
+      id,
+      inputData,
+      this.defaultData,
+      process.env.BANDS_URL,
+    );
+  }
 }
